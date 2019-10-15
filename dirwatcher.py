2b5002f05a7f7ff31b56d69c3ede51199af3d3cf
Shaquon <shaquonkelley@gmail.com>
2019-10-15 11:02:26 -0400

    Worked on linting errors and bugs in find_magic
    Morgan helped me by showing me that I needed my linter on. Saw a lot of bugs dealing with spelling and formatting. Also found a few bugs in find_magic. Needed to use global on the exit_flag. Also fixed the log.info for when it says: now tracking .txt

--- dirwatcher.py
+++ dirwatcher.py
@@ -2,41 +2,40 @@
 # -*- coding: utf-8 -*-
 
 """
-This is a program that watches a chosen directory and searches the the text for a "magic" word.
-Log when magic word is found. 
+- This is a program that watches a chosen directory and searches
+ the text for a "magic" word.
+- Log when magic word is found.
 """
 
-__author__ = "Shaquon with help from Piero" 
+__author__ = "Shaquon with help from Piero"
 
 import time
 import argparse
 import signal
 import logging
-import os 
+import os
 
 logging.basicConfig(
-format='%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s %(message)s',
-datefmt='%Y-%m-%d %H:%M:%S',
-level=logging.DEBUG
+    format='%(asctime)s.%(msecs)03d %(name)-12s %(levelname)-8s %(message)s',
+    datefmt='%Y-%m-%d %H:%M:%S',
+    level=logging.DEBUG
 )
-
 exit_flag = False
 logger = logging.getLogger(__file__)
 tracking_dict = {}
 
+
 def watch_directory(dir_to_watch, file_ext, search_text):
     dir_files = os.listdir(dir_to_watch)
-
     for file in dir_files:
         if file.endswith(file_ext) and file not in tracking_dict:
             tracking_dict[file] = 0
-            
-            logger.info("Now tracking {}".format(__file__))
+            logger.info("Now tracking {}".format(file))
 
     for file in list(tracking_dict):
         if file not in dir_files:
-            del tracking_dict[file]
             logger.info("{} removed from watchlist.".format(file))
+            del tracking_dict[file]
 
     for file in tracking_dict:
         tracking_dict[file] = find_magic(
@@ -45,17 +44,20 @@ def watch_directory(dir_to_watch, file_ext, search_text):
             search_text
         )
 
+
 def find_magic(file, start_pos, magic_word):
     line_number = 0
     with open(file) as f:
         for line_number, line in enumerate(f):
             if line_number >= start_pos:
                 if magic_word in line:
-                    l = line_number+1
+                    new_line = line_number + 1
                     logger.info(
-                        "{}: found '{}' on line {}".format(file, magic_word, l)
+                        "{}: found '{}' on line {}".format(
+                            file, magic_word, new_line)
                     )
     return line_number+1
+
 
 def signal_handler(sig_num, frame):
     """
@@ -68,10 +70,13 @@ def signal_handler(sig_num, frame):
     # log the associated signal name (the python3 way)
     logger.warn('Received ' + signal.Signals(sig_num).name)
     # log the signal name (the python2 way)
-    signames = dict((k, v) for v, k in reversed(sorted(signal.__dict__.items()))
- if v.startswith('SIG') and not v.startswith('SIG_'))
+    signames = dict((k, v) for v, k in reversed(
+                    sorted(signal.__dict__.items()))
+                    if v.startswith('SIG') and not v.startswith('SIG_'))
     logger.warn('Received ' + signames[sig_num])
+    global exit_flag
     exit_flag = True
+
 
 def create_parser():
     """Creates parser and setup cmd line options"""
@@ -86,19 +91,16 @@ def create_parser():
     parser.add_argument("magic", help="magic word to be found")
     return parser
 
+
 def main():
     parser = create_parser()
     args = parser.parse_args()
 
-
-    # Hook these two signals from the OS .. 
     signal.signal(signal.SIGINT, signal_handler)
     signal.signal(signal.SIGTERM, signal_handler)
-    # Now my signal_handler will get called if OS sends either of these to my process.
 
     start_time = time.time()
 
-    #startup banner
     logger.info(
         '\n'
         '-------------------------------------------------------------------\n'
@@ -108,8 +110,11 @@ def main():
         .format(__file__, start_time)
     )
     logger.info(
-        "Scanning path: {}, for files with extension: {}, that contain the magic word: {}".format(args.dir, args.ext, args.magic)
-    )
+        'Scanning path: {}, '
+        'for files with extension: {},'
+        'that contain the magic word: {}'
+        .format(args.dir, args.ext, args.magic)
+                )
 
     while not exit_flag:
         try:
@@ -121,19 +126,19 @@ def main():
             logger.error('{} Unhandled Exception'.format(str(e)))
 
         time.sleep(int(args.interval))
-    
+
     end_time = time.time()
-    # final exit point happens here
-    # Log a message that we are shutting down
-    # Include the overall uptime since program start.
-    logger.info("\n"
-        "----------------------------------------------------\n"
-        "Stopped watching\n"
-        'Uptime was ' + str(int(end_time-start_time)) + ' seconds\n'
-        "----------------------------------------------------\n"
-        )
+
+    logger.info(
+                '\n'
+                '----------------------------------------------------\n'
+                'Stopped watching\n'
+                'Uptime was ' + str(int(end_time-start_time)) + ' seconds\n'
+                '----------------------------------------------------\n'
+                )
 
     logging.shutdown()
+
 
 if __name__ == '__main__':
     print('file is being run directly')
